6d842cbde3dddb22014217b899eee6272a80896a
yvan <osamyvchris@yahoo.fr>
2015-05-06 01:53:22 +0200

    Rendu tp Jpa+Servlet

---
+++ src/main/java/fr/istic/tpjpa/domain/Chauffage.java
@@ -0,0 +1,29 @@
+package fr.istic.tpjpa.domain;
+
+
+import fr.istic.tpjpa.domain.*;
+
+import javax.persistence.Entity;
+
+
+
+
+@Entity
+public class Chauffage extends Peripherique
+{
+
+	public Chauffage() 
+	{
+		// TODO Auto-generated constructor stub
+		super();
+	}
+	//9876543210L;
+	
+	
+	
+
+
+	
+	
+
+}
---
+++ src/main/java/fr/istic/tpjpa/domain/ElectricDevice.java
@@ -0,0 +1,22 @@
+package fr.istic.tpjpa.domain;
+
+import fr.istic.tpjpa.domain.Peripherique;
+
+import javax.persistence.Entity;
+
+@Entity
+public class ElectricDevice extends Peripherique
+
+{
+	
+	
+
+	public ElectricDevice()
+	{
+		super();
+	}
+	
+	
+	
+
+}
